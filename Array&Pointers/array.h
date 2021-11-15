cc7fc17c3902b93024a229324e914c8a79275763
ormi.shur <omrishur@gmail.com>
2021-11-15 16:15:20 +0200

    backup

---
+++ Array&Pointers/array.h
@@ -0,0 +1,13 @@
+#ifndef  __ARRAY_H__
+#define __ARRAY_H__
+
+//recieves a matrix and calculates the sum of each row, sums of rows should be saved in result array, passed by user, return 0 if successful.
+int MtxSum(int row, int col, const int matrix [row][col], int arr[row]);
+//calculate last man standing position in josephus game.
+int Lucky(int num);
+//prints to stdout sizes of all data types
+//int DTypeNames();
+//copies all the envirnoment variables into char** buffer in lower case, and prints them
+//int PrintEnv();
+
+#endif
---
+++ Array&Pointers/main.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include "array.h"
+
+int main()
+{
+
+	/************************************************/
+	/***********************Sum**********************/
+	/************************************************/
+
+	printf ("------------------------SumMtx Test-------------------------\n");
+
+	int row = 10;
+	int col = 14;
+	int mtx[row][col];
+	
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++) 
+		{
+			mtx[i][j] = 10;
+		}
+	}
+	
+	for (int i = 0; i < row; i++)
+	{
+		mtx[2][i] = 3;
+	}
+	mtx[9][9] = 670;
+	for (int i = 0; i < row; i++)
+	{
+		for (int j = 0; j < col; j++) 
+		{
+			printf("%d  ", mtx[i][j]);
+		}
+		printf("\n");
+	}
+	
+	int sumarr[row];
+	MtxSum (row, col, mtx, sumarr);
+	for (int j = 0; j < row; j++) 
+		{
+			printf("****%d  ", sumarr[j]);
+		}
+		printf("\n");
+	
+	
+	/************************************************/
+	/*******************Josephus*********************/
+	/************************************************/
+	
+	printf ("------------------------Josephus Test-------------------------\n");
+	
+	int check = Lucky(14);
+	printf("\nfor check = 14 - result should be 13\n");
+	printf("result - %d\n", check);
+	
+	printf ("\n----------------\n");
+	    
+	check = Lucky(430);
+	printf("\nfor check = 430 - result should be 349\n");
+	printf("result - %d\n", check);
+	
+	printf ("\n----------------\n");
+	
+	check = Lucky(142);
+	printf("\nfor check = 142 - result should be 29\n");
+	printf("result - %d\n", check);
+	
+	
+}
